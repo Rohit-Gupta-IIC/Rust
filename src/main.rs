9b63e0af74d9df6233cef572dff2568935580315
Rohit-Gupta-IIC <rohit.gupta@iic.ac.in>
2023-03-02 21:03:24 +0530

    2 Feb 2023

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    const MY_VAR: u8 = 8;
+    const _MY_VAR: u8 = 8;
     static MY_STATIC_VAR: u8 = 4;
-    println!("{}",MY_STATIC_VAR);
+    println!("{}", MY_STATIC_VAR);
+
+    let my_string = String::from("Rohit Gupta");
+    let my_str: str = &my_string[4..9];
+    println!("{}", my_str[5]);
+
+    let my_arr: [usize;5] = [1, 2, 3, 4, 5];
+    let my_arra:[u8;5] = [1, 2];
 }
